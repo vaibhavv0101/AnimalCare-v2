179e68463ff989faf81c429436b97e129c1a466d
Vaibhav <your-email@example.com>
2026-08-02 07:41:21 +0530

    Complete authentication module with JWT and protected routes

--- backend/app/auth/routes.py
+++ backend/app/auth/routes.py
@@ -3,6 +3,8 @@ from flask import request, jsonify
 from app.auth import auth_bp
 from app.auth.schemas import RegisterSchema, LoginSchema
 from app.auth.service import register_user, login_user
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from app.models.user import User
 
 register_schema = RegisterSchema()
 login_schema = LoginSchema()
@@ -40,3 +42,21 @@ def login():
     response, status = login_user(request.json)
 
     return jsonify(response), status
+
+@auth_bp.route("/profile", methods=["GET"])
+@jwt_required()
+def profile():
+
+    user_id = get_jwt_identity()
+
+    user = User.query.get(user_id)
+
+    return {
+        "success": True,
+        "user": {
+            "id": user.id,
+            "full_name": user.full_name,
+            "email": user.email,
+            "role": user.role.name
+        }
+    }
