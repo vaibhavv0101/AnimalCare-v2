d7ac760b26490178ed7241bf9f10fc4bbf422c14
Vaibhav <your-email@example.com>
2026-08-02 06:13:45 +0530

    Initial project setup

---
+++ backend/app/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+
+from app.config import Config
+from app.extensions import db, migrate, jwt, cors
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    jwt.init_app(app)
+    cors.init_app(app)
+
+    @app.route("/")
+    def home():
+        return {
+            "project": "AnimalCare",
+            "version": "2.0",
+            "status": "Backend Running Successfully"
+        }
+
+    return app
