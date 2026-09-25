51b58261a87997f111fe1f90b1e71f349847e3e7
Vaibhav <your-email@example.com>
2026-09-25 07:36:03 +0530

    Fix Railway MySQL connection using PyMySQL

--- backend/config.py
+++ backend/config.py
@@ -6,8 +6,11 @@ load_dotenv()
 
 class Config:
 
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
-
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "").replace(
+    "mysql://",
+    "mysql+pymysql://",
+    1
+)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
